566c7a3a8419d26d9ea18bc2a5f7742423c50622
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-24 23:27:04 -0700

    add

---
+++ main.js
@@ -0,0 +1,38 @@
+// object task
+
+
+var library = [
+{
+	author : 'Bill Gates',
+	
+	title : 'The Road Ahead',
+	
+	readingStatus :'true',
+},
+{
+	author : 'steve jobs',
+	
+	title : 'walter isaacson',
+	
+	readingStatus :'true',
+	
+},
+{
+	author : 'suzzanne collins',
+	
+	title : 'Mockingjay: The Final Book of the Hunger Games',
+	
+	readingStatus :'false',
+}]
+
+	console.log(library[0].author);
+	console.log(library[0].title);
+	console.log(library[0].readingStatus);
+	
+	console.log(library[1].author);
+	console.log(library[1].title);
+	console.log(library[1].readingStatus);
+	
+	console.log(library[2].author);
+	console.log(library[2].title);
+	console.log(library[2].readingStatus);
